2469bd5322ce7a28ac469cc805a51d590c656751
pratheek <pratheek.mukkavilli@gmail.com>
2026-03-28 22:18:17 -0500

    fbv

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -12,13 +12,13 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.2.0"),
-        .package(name: "CapacitorApp", path: "../../node_modules/@capacitor/app"),
-        .package(name: "CapacitorCamera", path: "../../node_modules/@capacitor/camera"),
-        .package(name: "CapacitorFilesystem", path: "../../node_modules/@capacitor/filesystem"),
-        .package(name: "CapacitorPushNotifications", path: "../../node_modules/@capacitor/push-notifications"),
-        .package(name: "CapacitorShare", path: "../../node_modules/@capacitor/share"),
-        .package(name: "CapacitorStatusBar", path: "../../node_modules/@capacitor/status-bar"),
-        .package(name: "RevenuecatPurchasesCapacitor", path: "../../node_modules/@revenuecat/purchases-capacitor")
+        .package(name: "CapacitorApp", path: "../../../node_modules/@capacitor/app"),
+        .package(name: "CapacitorCamera", path: "../../../node_modules/@capacitor/camera"),
+        .package(name: "CapacitorFilesystem", path: "../../../node_modules/@capacitor/filesystem"),
+        .package(name: "CapacitorPushNotifications", path: "../../../node_modules/@capacitor/push-notifications"),
+        .package(name: "CapacitorShare", path: "../../../node_modules/@capacitor/share"),
+        .package(name: "CapacitorStatusBar", path: "../../../node_modules/@capacitor/status-bar"),
+        .package(name: "RevenuecatPurchasesCapacitor", path: "../../../node_modules/@revenuecat/purchases-capacitor")
     ],
     targets: [
         .target(
